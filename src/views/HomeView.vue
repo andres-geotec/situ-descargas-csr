2de9ed4ae298f250312a19145a9666d09bf6eb65
Angel06T <94639264+Angel06T@users.noreply.github.com>
2025-01-17 17:16:55 -0600

    Update HomeView.vue
    targeta de decarga

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup></script>
+<script setup>
+import TragetasDescarga from '@/components/TragetasDescarga.vue'
+</script>
 
 <template>
   <main class="contenedor">
     <div class="ancho-fijo">
       <h1 class="texto-centrado">Descargas</h1>
+      <TragetasDescarga></TragetasDescarga>
     </div>
   </main>
 </template>
