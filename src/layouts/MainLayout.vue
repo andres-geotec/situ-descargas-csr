b80cd09d1062851017ff031e64a9bb9fa7a355c3
andres-geotec <andres.geotec@gmail.com>
2025-02-17 23:38:24 -0600

    🔧 feat: integrando menú de acesibilidad

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,17 @@
+<script setup>
+import { useAccesibilidadStore } from '@centrogeomx/sisdai-componentes/src/stores'
+
+const storeAccesibilidad = useAccesibilidadStore()
+</script>
+
 <template>
   <header>
     <SisdaiNavegacionGobMx />
   </header>
 
   <RouterView />
+
+  <SisdaiMenuAccesibilidad :objetoStore="storeAccesibilidad" />
 
   <footer class="p-b-0">
     <SisdaiPiePaginaGobMx />
